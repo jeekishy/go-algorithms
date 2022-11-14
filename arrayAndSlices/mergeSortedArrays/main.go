bd416a93b3201950b7850df7bd33064590c9c721
Kish Jeetun <jeetun@gmail.com>
2022-11-14 16:43:43 +1000

    merge sorted arrays

--- arrayAndSlices/mergeSortedArrays/main.go
+++ arrayAndSlices/mergeSortedArrays/main.go
@@ -1,16 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	slice1 := []int{0, 3, 4, 51}
 	slice2 := []int{4, 6, 50}
 
-	result := mergeSortedArrays(slice1, slice2)
-	fmt.Println(result)
+	result1 := mergeSortedArrays(slice1, slice2)
+	fmt.Println(result1)
 }
 
+// merge sorted arrays
 func mergeSortedArrays(a1, a2 []int) []int {
+	mergedArray := make([]int, 0)
 
-	return []int{}
+	// merge both array into one
+	mergedArray = append(mergedArray, a1[:]...)
+	mergedArray = append(mergedArray, a2[:]...)
+
+	// sort merged array
+	sort.Sort(sort.IntSlice(mergedArray))
+
+	return mergedArray
 }
